0501912aabc80508ac0621775f86110e0f9d0a9a
IonathanG <ionathangusmini@outlook.com>
2022-06-23 23:49:07 +0800

    added firestore init from redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { UserProvider } from "./context/UserContext";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -8,25 +7,34 @@ import ProductPage from "./pages/ProductPage";
 import Register from "./pages/Register";
 import Announcement from "./components/Announcement";
 import Navbar from "./components/Navbar";
+import { useContext, useEffect } from "react";
+import UserContext from "./context/UserContext";
+import { getUserData } from "./feature/cartSlice";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const { user } = useContext(UserContext);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) dispatch(getUserData(user.uid));
+  }, [user, dispatch]);
+
   return (
     <div className="app">
-      <UserProvider>
-        <BrowserRouter>
-          <Announcement />
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/product-list" element={<ProductList />} />
-            <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </UserProvider>
+      <BrowserRouter>
+        <Announcement />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product-list" element={<ProductList />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
