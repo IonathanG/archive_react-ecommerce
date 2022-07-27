04d02ff8f9e5739d580b2c6630c1fde68135ce8b
IonathanG <ionathangusmini@outlook.com>
2022-07-27 18:12:46 +0800

    useMemo added to products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,34 +1,34 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { popularProducts } from "../data";
 import Product from "./Product";
 
 const Products = ({ type = "all", sortBy = "DEFAULT" }) => {
-  return (
-    <div className="products-container">
-      {popularProducts
-        .filter((item) => {
-          if (type !== "all") {
-            return item.type === type;
-          } else {
-            return true;
-          }
-        })
-        .sort((a, b) => {
-          if (sortBy.toUpperCase() === "DEFAULT") {
-            return b.id - a.id;
-          } else if (sortBy.toUpperCase() === "PRICE_UP") {
-            return a.price - b.price;
-          } else if (sortBy.toUpperCase() === "PRICE_DOWN") {
-            return b.price - a.price;
-          } else {
-            return null;
-          }
-        })
-        .map((item) => (
-          <Product item={item} key={item.id} />
-        ))}
-    </div>
-  );
+  // useMemo to sort out and filter items
+  // avoid uneccessary computing
+  const productsDisplayed = useMemo(() => {
+    return popularProducts
+      .filter((item) => {
+        if (type !== "all") {
+          return item.type === type;
+        } else {
+          return true;
+        }
+      })
+      .sort((a, b) => {
+        if (sortBy.toUpperCase() === "DEFAULT") {
+          return b.id - a.id;
+        } else if (sortBy.toUpperCase() === "PRICE_UP") {
+          return a.price - b.price;
+        } else if (sortBy.toUpperCase() === "PRICE_DOWN") {
+          return b.price - a.price;
+        } else {
+          return null;
+        }
+      })
+      .map((item) => <Product item={item} key={item.id} />);
+  }, [type, sortBy]);
+
+  return <div className="products-container">{productsDisplayed}</div>;
 };
 
 export default Products;
