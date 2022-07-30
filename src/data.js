5b83ae02462e4812f94e5019c9caed470cdf263e
IonathanG <ionathangusmini@outlook.com>
2022-07-30 15:45:01 +0800

    rpopuroduct link updated

--- src/data.js
+++ src/data.js
@@ -44,7 +44,8 @@ export const popularProducts = [
   {
     id: 0,
     name: "Denim T-Shirt",
-    img: "http://cdn.shopify.com/s/files/1/0635/7395/0708/products/404-page-not-found-t-shirt-polycoton-unisexe-291621.png?v=1654770601",
+    img: "products/tshirt_01.webp",
+    //img: "http://cdn.shopify.com/s/files/1/0635/7395/0708/products/404-page-not-found-t-shirt-polycoton-unisexe-291621.png?v=1654770601",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, porro? Animi, nobis harum dolor architecto tempore in dicta et repudiandae? Sit consequuntur in magni suscipit eveniet quam voluptatem repudiandae velit?",
     price: 20,
     type: "t-shirt",
@@ -52,7 +53,8 @@ export const popularProducts = [
   {
     id: 1,
     name: "Denim Shirt",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5mtR_28A1uTccBViflpf-3LcxUE0zTYfDqQ&usqp=CAU",
+    img: "products/shirt_01.jpeg",
+    //img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5mtR_28A1uTccBViflpf-3LcxUE0zTYfDqQ&usqp=CAU",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, porro? Animi, nobis harum dolor architecto tempore in dicta et repudiandae? Sit consequuntur in magni suscipit eveniet quam voluptatem repudiandae velit?",
     price: 30,
     type: "shirt",
@@ -60,7 +62,8 @@ export const popularProducts = [
   {
     id: 2,
     name: "Denim Shirt",
-    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    img: "products/shirt_02.png",
+    //img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, porro? Animi, nobis harum dolor architecto tempore in dicta et repudiandae? Sit consequuntur in magni suscipit eveniet quam voluptatem repudiandae velit?",
     price: 25,
     type: "shirt",
@@ -68,7 +71,8 @@ export const popularProducts = [
   {
     id: 3,
     name: "Denim Dress",
-    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    img: "products/dress_01.png",
+    //img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, porro? Animi, nobis harum dolor architecto tempore in dicta et repudiandae? Sit consequuntur in magni suscipit eveniet quam voluptatem repudiandae velit?",
     price: 35,
     type: "dress",
@@ -76,7 +80,8 @@ export const popularProducts = [
   {
     id: 4,
     name: "Denim Bag",
-    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    img: "products/bag_01.png",
+    //img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, porro? Animi, nobis harum dolor architecto tempore in dicta et repudiandae? Sit consequuntur in magni suscipit eveniet quam voluptatem repudiandae velit?",
     price: 10,
     type: "bag",
@@ -84,7 +89,8 @@ export const popularProducts = [
   {
     id: 5,
     name: "Denim Hat",
-    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    img: "products/hat_01.png",
+    //img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, porro? Animi, nobis harum dolor architecto tempore in dicta et repudiandae? Sit consequuntur in magni suscipit eveniet quam voluptatem repudiandae velit?",
     price: 55,
     type: "hat",
@@ -92,7 +98,8 @@ export const popularProducts = [
   {
     id: 6,
     name: "Denim T-Shirt",
-    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    img: "products/tshirt_02.png",
+    //img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, porro? Animi, nobis harum dolor architecto tempore in dicta et repudiandae? Sit consequuntur in magni suscipit eveniet quam voluptatem repudiandae velit?",
     price: 65,
     type: "t-shirt",
@@ -100,7 +107,8 @@ export const popularProducts = [
   {
     id: 7,
     name: "Denim Coat",
-    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    img: "products/coat_01.png",
+    //img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, porro? Animi, nobis harum dolor architecto tempore in dicta et repudiandae? Sit consequuntur in magni suscipit eveniet quam voluptatem repudiandae velit?",
     price: 33,
     type: "coat",
